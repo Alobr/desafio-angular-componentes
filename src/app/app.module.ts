e602799eb93a2f250c6a141a8b481b60033a18c0
Alonso Bracho <alonsoae80@gmail.com>
2022-11-21 21:40:14 -0300

    update del contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { AppContentComponent } from './app-content/app-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent,
+    AppNavbarComponent,
+    AppContentComponent
   ],
   imports: [
     BrowserModule
